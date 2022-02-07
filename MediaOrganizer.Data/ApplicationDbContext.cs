66b4f6f99bf73369294ff5f92ae60a0494712e47
Adam Metcalf <adam@tenaciousdev.com>
2022-02-07 16:45:41 -0500

    added tool manifest; installed tool dotnet-ef

--- MediaOrganizer.Data/ApplicationDbContext.cs
+++ MediaOrganizer.Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediaOrganizer.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace MediaOrganizer.Data
@@ -10,5 +11,8 @@ namespace MediaOrganizer.Data
   {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+    public DbSet<MediaCatalog> MediaCatalogs { get; set; }
+    public DbSet<MediaObject> MediaObjects { get; set; }
+    public DbSet<MediaType> MediaTypes { get; set; }
   }
 }
